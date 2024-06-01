7b805000f17b1aa3bc845163841fe3e44fd4cc63
Van Husein <muhakbarhusein5@gmail.com>
2024-06-01 13:06:38 +0700

    "Added 'next()' call to error handler middleware"

--- controllers/auth/login.controller.ts
+++ controllers/auth/login.controller.ts
@@ -2,6 +2,12 @@ import type { Request, Response, NextFunction } from "express"
 
 const loginhandler = (req: Request, res: Response, next: NextFunction) => {
     const { username, email, password } = req.body
+
+    try {
+        throw new Error('tes')
+    } catch (error) {
+        next(error)
+    }
 }
 
 export default loginhandler

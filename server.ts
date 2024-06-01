1c93ec9a8e52156b9b0cf83ed84abfd4cb3a9cbb
Van Husein <muhakbarhusein5@gmail.com>
2024-06-01 12:37:33 +0700

    Use bodyparser

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import express, { Express } from 'express'
+import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import dotenv from 'dotenv'
 import router from './routes'
@@ -10,6 +11,7 @@ const app: Express = express()
 const host = process.env.HOST
 const port = process.env.PORT
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(morgan('tiny'))
 app.use('/api', router)
 app.use(errorhandler)

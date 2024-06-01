39c4b870ea26574c4512b71ec6e09a822d76f476
Van Husein <muhakbarhusein5@gmail.com>
2024-06-01 12:46:13 +0700

    Added email existence check before creating a new user.

--- controllers/auth/register.controller.ts
+++ controllers/auth/register.controller.ts
@@ -7,11 +7,18 @@ const registerhandler = async (req: Request, res: Response, next: NextFunction) 
     const { username, email, password } = req.body;
 
     try {
+        const emailIsExists = await pc.user.findFirst({ where: { email } })
+
+        if (emailIsExists) {
+            return res.status(status.BAD_REQUEST).json({
+                message: 'Email already exists'
+            })
+        }
+
         const hashPassword = hashSync(password, 10)
         const user = await pc.user.create({
             data: { username, email, password: hashPassword, },
         });
-
 
         if (user) {
             res.status(status.CREATED).json({

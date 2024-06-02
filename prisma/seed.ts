c00cf3809bad2c1fcfae938b5e10ed0a8784220b
Van Husein <muhakbarhusein5@gmail.com>
2024-06-02 16:40:31 +0700

    "Updated package.json and prisma schema: added prisma seed script, removed comments and models Categoriess and Theme, and modified models User and Note"

---
+++ prisma/seed.ts
@@ -0,0 +1,67 @@
+import pc from '../utils/prisma'
+
+async function main() {
+    const user = await pc.user.create({
+        data: {
+            username: 'vanhusein',
+            email: 'vanhusein@gmail.com',
+            password: 'password',
+        },
+    })
+
+    console.log('[----------------------- Seeding User -----------------------]')
+    console.log(user)
+
+    const notes = await pc.note.createMany({
+        data: [
+            {
+                title: 'Note 1',
+                content: 'Note 1 content',
+                user_email: 'vanhusein@gmail.com'
+            },
+            {
+                title: 'Note 2',
+                content: 'Note 2 content',
+                user_email: 'vanhusein@gmail.com'
+            }
+        ]
+    })
+
+    console.log('[----------------------- Seeding Notes -----------------------]')
+    console.log(notes)
+
+    // const categoriess = await pc.categoriess.createMany({
+    //     data: [
+    //         {
+    //             name: 'Hari Saya',
+    //             icon: 'ri-sun-line',
+    //         },
+    //         {
+    //             name: 'Penting',
+    //             icon: 'ri-star-line',
+    //         },
+    //         {
+    //             name: 'Terencana',
+    //             icon: 'ri-calendar-2-line',
+    //         },
+    //         {
+    //             name: 'Tugas',
+    //             icon: 'ri-checkbox-line',
+    //         },
+
+    //     ]
+    // })
+    // console.log('[----------------------- Seeding Categoriess -----------------------]')
+    // console.log(categoriess)
+
+}
+
+main()
+    .then(async () => {
+        await pc.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await pc.$disconnect()
+        process.exit(1)
+    })

fda927682370f3f3d472f7e0047c076e8dbb24c7
Van Husein <muhakbarhusein5@gmail.com>
2024-06-03 09:13:05 +0700

    "Removed imports for 'verify' and 'decode' from 'jsonwebtoken', added a blank line, and removed no other changes."

--- config.ts
+++ config.ts
@@ -1,9 +1,10 @@
-import { sign, verify, decode } from 'jsonwebtoken'
+import { sign } from 'jsonwebtoken'
 
 export type signing = {
     username: string,
     email: string
 }
+
 
 const signing = (signing: signing) => {
     const token = sign(signing, process.env.JWT_SECRET as string, {
